e8c95f683e6e31ebc6fafadb635172a64c1748bb
Dave Syer <dsyer@vmware.com>
2024-04-23 08:51:25 +0100

    Add sample (not using templates yet)

---
+++ sample/src/main/java/org/springframework/experimental/SampleController.java
@@ -0,0 +1,47 @@
+package org.springframework.experimental;
+
+import java.util.Date;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SampleController {
+
+	@GetMapping(path = "/")
+	String user(Map<String, Object> model) {
+		model.put("message", "Welcome");
+		model.put("time", new Date());
+		return "index";
+	}
+
+	@GetMapping(path = "/greet")
+	String greet(Map<String, Object> model) {
+		model.put("greeting", "Hello World");
+		model.put("time", new Date());
+		return "greet";
+	}
+
+	@GetMapping(path = "/logo")
+	String logo() {
+		return "layout :: logo";
+	}
+
+	@PostMapping(path = "/greet")
+	String name(Map<String, Object> model, @RequestParam String name) {
+		greet(model);
+		model.put("greeting", "Hello " + name);
+		model.put("name", name);
+		return "greet";
+	}
+
+	@PostMapping(path = "/greet", headers = "hx-request=true")
+	String nameHtmx(Map<String, Object> model, @RequestParam String name) {
+		name(model, name);
+		return "greet :: content";
+	}
+
+}
---
+++ sample/src/main/java/org/springframework/experimental/WebmvcApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.experimental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmvcApplication.class, args);
+	}
+
+}
